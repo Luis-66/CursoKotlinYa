8a3f6ef790017728aefeae0adc2bc1fdb5751c57
Luis-44 <nirvana_luis_rock@hotmail.com>
2022-10-27 00:50:00 -0500

    Primer commit del curso de Kotlin Ya 003 Tipos de variables

---
+++ src/main/kotlin/003 Tipos de Variables.kt
@@ -0,0 +1,13 @@
+//`003 Tipos de Variables`
+/*
+* Una variable es un depósito donde hay un valor. Consta de un nombre y
+* pertenece a un tipo.
+* En el lenguaje Kotlin si necesitamos almacenar un valor numérico entero
+* podemos definir una variable de tipo:
+* Byte
+  Short
+  Int
+  Long
+* */
+fun main() {
+}
