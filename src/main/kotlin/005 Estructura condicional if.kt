f286552cd42730a52e517ccbc0b37ce5bb3c0d69
Luis-44 <nirvana_luis_rock@hotmail.com>
2022-11-03 00:08:53 -0600

    005 Condicional if

---
+++ src/main/kotlin/005 Estructura condicional if.kt
@@ -0,0 +1,50 @@
+/* 5 - Estructura condicional if
+Cuando hay que tomar una decisión aparecen las estructuras condicionales.
+En nuestra vida diaria se nos presentan situaciones donde debemos decidir.
+¿Elijo la carrera A o la carrera B?
+¿Me pongo este pantalón?
+Para ir al trabajo, ¿elijo el camino A o el camino B?
+Al cursar una carrera, ¿elijo el turno mañana, tarde o noche?
+
+Estructura condicional simple
+Cuando se presenta la elección tenemos la opción de realizar una actividad o
+no realizar ninguna
+
+ */
+
+fun main(){
+
+}
+private fun problema1(){
+    /*
+Ingresar el sueldo de una persona, si supera los 3000 pesos mostrar un mensaje
+en pantalla indicando que debe abonar impuestos.
+    */
+
+}
+private fun problema2(){
+    /*Realizar un programa que solicite ingresar dos números enteros distintos y
+    muestre por pantalla el mayor de ellos (suponemos que el operador del programa
+    ingresa valores distintos, no valida nuestro programa dicha situación)
+
+     */
+}
+private fun problema3(){
+    /*
+Se ingresan por teclado 2 valores enteros. Si el primero es menor al segundo calcular
+la suma y la resta, luego mostrarlos, sino calcular el producto y la división.
+     */
+}
+private fun ejercicio1(){
+    /*
+Se ingresan tres notas de un alumno, si el promedio es mayor o igual a siete mostrar un
+mensaje "Promocionado".
+     */
+}
+private fun ejercicio2(){
+    /*
+Se ingresa por teclado un número entero comprendido entre 1 y 99, mostrar un
+mensaje indicando si el número tiene uno o dos dígitos.
+(Tener en cuenta que condición debe cumplirse para tener dos dígitos, un número entero)
+     */
+}
