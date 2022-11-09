25805495fe99ebd6d56eac835348e9aa42153d03
Luis-44 <nirvana_luis_rock@hotmail.com>
2022-11-09 00:36:20 -0600

    006 Condicional if Como Expresion

---
+++ src/main/kotlin/006 Estructura condicional if como Expresión.kt
@@ -0,0 +1,35 @@
+/*
+6 - Estructura condicional if como expresión
+En Kotlin existe la posibilidad de que la estructura condicional if retorne un valor,
+característica no común a otros lenguajes de programación.
+
+Veremos con una serie de ejemplos la sintaxis para utilizar el if como expresión.
+
+ */
+
+fun main(){
+
+}
+private fun problema1(){
+    /*
+    Cargar dos valores enteros, almacenar el mayor de los mismos en otra
+    variable y mostrarlo.
+    */
+
+}
+private fun problema2(){
+    /*
+    Ingresar por teclado un valor entero. Almacenar en otra variable el cuadrado de
+    dicho número si el valor ingresado es par, en caso que sea impar guardar el cubo.
+    Mostrar además un mensaje que indique si se calcula el cuadrado o el cubo.
+     */
+}
+private fun ejercicio1(){
+    /*
+    Cargar un valor entero por teclado comprendido entre 1 y 99.
+    Almacenar en otra variable la cantidad de dígitos que tiene el valor ingresado
+    por teclado.
+    Mostrar la cantidad de dígitos del número ingresado por teclado.
+     */
+}
+
