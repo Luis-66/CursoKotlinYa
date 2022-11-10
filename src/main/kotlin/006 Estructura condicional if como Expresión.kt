0e60a4f770843c27ac7f2cda8ce4d0358f47e494
Luis-44 <nirvana_luis_rock@hotmail.com>
2022-11-09 23:41:45 -0600

    006 Condicional if Como Expresion-Resuelto

--- src/main/kotlin/006 Estructura condicional if como Expresión.kt
+++ src/main/kotlin/006 Estructura condicional if como Expresión.kt
@@ -8,13 +8,22 @@ Veremos con una serie de ejemplos la sintaxis para utilizar el if como expresió
  */
 
 fun main(){
-
+    //problema1()
+    //problema2()
+    ejercicio1()
 }
 private fun problema1(){
     /*
     Cargar dos valores enteros, almacenar el mayor de los mismos en otra
     variable y mostrarlo.
     */
+    println("Ingrese el primer valor")
+    val valor1 = readln()!!.toInt()
+    println("Ingrese el segundo valor")
+    val valor2 = readln()!!.toInt()
+
+    val valorMayor = if (valor1 > valor2) valor1 else valor2
+    println("El numero mayor es $valorMayor")
 
 }
 private fun problema2(){
@@ -23,6 +32,19 @@ private fun problema2(){
     dicho número si el valor ingresado es par, en caso que sea impar guardar el cubo.
     Mostrar además un mensaje que indique si se calcula el cuadrado o el cubo.
      */
+    println("Ingrese un valor entero")
+    val valor1 = readln()!!.toInt()
+
+    val result = if (valor1 % 2 == 0){
+        println("Cuadrado")
+        valor1 * valor1
+    }else{
+
+        println("Cubo")
+        valor1 * valor1 * valor1
+    }
+    println("El resultado es: $result")
+
 }
 private fun ejercicio1(){
     /*
@@ -31,5 +53,12 @@ private fun ejercicio1(){
     por teclado.
     Mostrar la cantidad de dígitos del número ingresado por teclado.
      */
+    println("Ingrese un digito entre 1 y 99")
+    val numero = readln()!!.toInt()
+
+    val digitos = if (numero in 1..9) 1 else 2
+
+    println("Tiene $digitos")
+
 }
 
