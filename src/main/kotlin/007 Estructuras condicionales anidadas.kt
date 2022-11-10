d51c4355254f72728d8ba07cbc62bb84c0df43f1
Luis-44 <nirvana_luis_rock@hotmail.com>
2022-11-09 23:48:46 -0600

    007 Condicional if anidadas

---
+++ src/main/kotlin/007 Estructuras condicionales anidadas.kt
@@ -0,0 +1,56 @@
+/* 7 - Estructuras condicionales anidadas
+
+Decimos que una estructura condicional es anidada cuando por
+la rama del verdadero o el falso de una estructura condicional
+hay otra estructura condicional.
+
+
+ */
+
+fun main(){
+
+}
+private fun problema1(){
+    /*
+    Confeccionar un programa que pida por teclado tres notas de un alumno,
+    calcule el promedio e imprima alguno de estos mensajes:
+    Si el promedio es >=7 mostrar "Promocionado".
+    Si el promedio es >=4 y <7 mostrar "Regular".
+    Si el promedio es <4 mostrar "Reprobado".
+    */
+
+}
+private fun problema1_2(){
+    /*
+    Si utilizamos if como expresiones podemos codificar el mismo programa en forma más
+    concisa con el siguiente código:
+     */
+}
+
+private fun ejercicio1(){
+    /*
+    Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.
+     */
+}
+private fun ejercicio2(){
+    /*
+    Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el número
+    es positivo, nulo o negativo
+     */
+}
+private fun ejercicio3(){
+    /*
+    Confeccionar un programa que permita cargar un número entero positivo de hasta
+    tres cifras y muestre un mensaje indicando si tiene 1, 2, o 3 cifras.
+    Mostrar un mensaje de error si el número de cifras es mayor.
+     */
+}
+private fun ejercicio4(){
+    /*
+    Un postulante a un empleo, realiza un test de capacitación, se obtuvo la
+    siguiente información: cantidad total de preguntas que se le realizaron y la
+    cantidad de preguntas que contestó correctamente. Se pide confeccionar un
+    programa que ingrese los dos datos por teclado e informe el nivel del mismo
+    según el porcentaje de respuestas correctas que ha obtenido, y sabiendo que:
+     */
+}
