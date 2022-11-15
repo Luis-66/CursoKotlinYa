47a1564369c070653c5bd5dc4c546f178aeb6b3b
Luis-44 <nirvana_luis_rock@hotmail.com>
2022-11-15 00:46:31 -0600

    008 Condiciones Compuestas

---
+++ src/main/kotlin/008 Condiciones Compuestas Operadores.kt
@@ -0,0 +1,70 @@
+/* 8 - Condiciones compuestas con operadores lógicos
+
+Hasta ahora hemos visto los operadores:
+
+relacionales (>, <, >=, <= , ==, !=)
+matemáticos (+, -, *, /, %)
+pero nos están faltando otros operadores imprescindibles:
+
+lógicos (&&, ||)
+Estos dos operadores se emplean fundamentalmente en las estructuras condicionales
+para agrupar varias condiciones simples.
+ */
+
+fun main(){
+
+}
+private fun problema1(){
+    /*
+    Confeccionar un programa que lea por teclado tres números y nos muestre el mayor.
+    */
+
+}
+private fun problema2(){
+    /*
+Se carga una fecha (día, mes y año) por teclado.
+Mostrar un mensaje si corresponde al primer trimestre del año (enero, febrero o marzo)
+Cargar por teclado el valor numérico del día, mes y año. Ejemplo: dia:10 mes:2 año:2017.
+     */
+}
+
+private fun ejercicio1(){
+    /*
+    Realizar un programa que pida cargar una fecha cualquiera, luego verificar si
+    dicha fecha corresponde a Navidad.
+     */
+}
+private fun ejercicio2(){
+    /*
+Se ingresan tres valores por teclado, si todos son iguales calcular el cubo del
+número y mostrarlo.
+     */
+}
+private fun ejercicio3(){
+    /*
+Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10,
+ imprimir en pantalla la leyenda "Todos los números son menores a diez".
+     */
+}
+private fun ejercicio4(){
+    /*
+Se ingresan por teclado tres números, si al menos uno de los valores ingresados
+es menor a 10, imprimir en pantalla la leyenda "Alguno de los números es menor a diez"
+     */
+}
+private fun ejercicio5(){
+    /*
+Escribir un programa que pida ingresar la coordenada de un punto en el plano,
+es decir dos valores enteros x e y (distintos a cero).
+Posteriormente imprimir en pantalla en que cuadrante se ubica dicho punto.
+(1º Cuadrante si x > 0 Y y > 0 , 2º Cuadrante: x < 0 Y y > 0, etc.)
+     */
+}
+private fun ejercicio6(){
+    /*
+Escribir un programa en el cual: dada una lista de tres valores enteros
+ingresados por teclado se guarde en otras dos variables el menor y el mayor de esa lista.
+Utilizar el if como expresión para obtener el mayor y el menor.
+Imprimir luego las dos variables.
+     */
+}
