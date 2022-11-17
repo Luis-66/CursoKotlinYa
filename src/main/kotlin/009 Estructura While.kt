64ca30c46f486c4d57d235178c003c191b419263
Luis-66 <nirvana_luis_rock@hotmail.com>
2022-11-17 00:01:07 -0600

    009 Ciclo While

---
+++ src/main/kotlin/009 Estructura While.kt
@@ -0,0 +1,104 @@
+/* 9 - Estructura repetitiva while
+
+Hasta ahora hemos empleado estructuras SECUENCIALES y CONDICIONALES.
+Existe otro tipo de estructuras tan importantes como las anteriores que son
+las estructuras REPETITIVAS.
+
+Una estructura repetitiva permite ejecutar una instrucción o un conjunto de
+instrucciones varias veces.
+
+Una ejecución repetitiva de sentencias se caracteriza por:
+- La o las sentencias que se repiten.
+- El test o prueba de condición antes de cada repetición, que motivará que
+se repitan o no las sentencias.
+
+
+
+ */
+
+fun main() {
+
+}
+
+private fun problema1() {
+    /*
+Realizar un programa que imprima en pantalla los números del 1 al 100.
+    */
+
+}
+
+private fun problema2() {
+    /*
+Escribir un programa que solicite la carga de un valor positivo y nos muestre desde 1 hasta el valor ingresado
+de uno en uno.
+Ejemplo: Si ingresamos 30 se debe mostrar en pantalla los números del 1 al 30.
+     */
+}
+
+private fun problema3() {
+    /*
+Desarrollar un programa que permita la carga de 10 valores por teclado y nos muestre
+posteriormente la suma de los valores ingresados y su promedio.
+     */
+}
+private fun problema4() {
+    /*
+Una planta que fabrica perfiles de hierro posee un lote de n piezas.
+Confeccionar un programa que pida ingresar por teclado la cantidad de piezas a
+procesar y luego ingrese la longitud de cada perfil; sabiendo que la pieza cuya
+longitud esté comprendida en el rango de 1.20 y 1.30 son aptas.
+Imprimir por pantalla la cantidad de piezas aptas que hay en el lote.
+     */
+}
+
+private fun ejercicio1() {
+    /*
+Escribir un programa que solicite ingresar 10 notas de alumnos y nos informe
+cuántos tienen notas mayores o iguales a 7 y cuántos menores.
+
+     */
+}
+
+private fun ejercicio2() {
+    /*
+Se ingresan un conjunto de n alturas de personas por teclado (n se ingresa por teclado).
+Mostrar la altura promedio de las personas.
+     */
+}
+private fun ejercicio3() {
+    /*
+En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y $500,
+realizar un programa que lea los sueldos que cobra cada empleado e informe cuántos
+empleados cobran entre $100 y $300 y cuántos cobran más de $300.
+Además el programa deberá informar el importe que gasta la empresa en sueldos al personal.
+     */
+}
+
+private fun ejercicio4() {
+    /*
+Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc.
+(No se ingresan valores por teclado)
+     */
+}
+private fun ejercicio5() {
+    /*
+Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16 - 24, etc.
+     */
+}
+
+private fun ejercicio6() {
+    /*
+Realizar un programa que permita cargar dos listas de 5 valores cada una.
+Informar con un mensaje cual de las dos listas tiene un valor acumulado mayor
+(mensajes "Lista 1 mayor", "Lista 2 mayor", "Listas iguales")
+Tener en cuenta que puede haber dos o más estructuras repetitivas en un algoritmo.
+     */
+}
+private fun ejercicio7() {
+    /*
+Desarrollar un programa que permita cargar n números enteros y luego nos
+informe cuántos valores fueron pares y cuántos impares.
+Emplear el operador "%" en la condición de la estructura condicional:
+	if (valor % 2 == 0)         //Si el if se verifica verdadero luego es par.
+     */
+}
